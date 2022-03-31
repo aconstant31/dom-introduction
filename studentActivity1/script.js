143193729713fcd776ff477251670273fca07953
AConstant31 <alexjconstantino31@gmail.com>
2022-03-30 20:22:28 -0400

    Completed SA1

---
+++ studentActivity1/script.js
@@ -0,0 +1,14 @@
+console.log("Test");
+
+let titleOne = document.getElementById("title-1");
+titleOne.style.color = "#ED6A5A";
+titleOne.style.fontSize = "50px";
+
+let paragraphOne = document.getElementById("paragraph-1");
+paragraphOne.style.fontSize = "20px";
+
+let listOne = document.getElementById("list-1");
+listOne.style.fontSize = "20px";
+
+let paragraphTwo = document.getElementById("paragraph-2")
+paragraphTwo.style.fontSize = "20px";
